74fba0444fa39b906702908f58b215704cbc2cef
Ezra Black <dev.ezrablack@gmail.com>
2020-04-27 13:16:59 -0500

    Base Project & files added

---
+++ Oxygen/Oxygen/Views/Cells/PlantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlantTableViewCell.swift
+//  Oxygen
+//
+//  Created by Ezra Black on 4/27/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Oxygen/Oxygen/Views/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  Oxygen
+//
+//  Created by Ezra Black on 4/27/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
