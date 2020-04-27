c6cc5beca3fa70d17cb3edf05eb534178c182610
Hunter Oppel <hunter.oppel@gmail.com>
2020-04-27 17:03:07 -0400

    Finished constraining storyboard and wiring buttons.

--- Oxygen/Oxygen/Views/RegisterViewController.swift
+++ Oxygen/Oxygen/Views/RegisterViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class RegisterViewController: UIViewController {
 
     // MARK: - Labels
+    
     @IBOutlet weak var createAccountLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var phoneNumberLabel: UILabel!
@@ -24,7 +25,7 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     
-    
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,15 +33,13 @@ class RegisterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // MARK: - Actions
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerAccount(_ sender: UIButton) {
+        
     }
-    */
-
+    
+    @IBAction func switchToLogin(_ sender: UIButton) {
+        
+    }
 }
