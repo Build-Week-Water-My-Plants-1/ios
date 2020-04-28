f8b0c9b07a8e05fe1d8f1c945ca754edfe1e8bbe
Harmony Radley <harmonyradley99@gmail.com>
2020-04-28 16:58:50 -0400

    Plant Detail View Controller

--- Oxygen/Oxygen/Views/Cells/PlantTableViewCell.swift
+++ Oxygen/Oxygen/Views/Cells/PlantTableViewCell.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class PlantTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var plantImage: UIImageView!
+    @IBOutlet weak var plantName: UILabel!
+    @IBOutlet weak var plantSpecies: UILabel! 
+    
+    
+    func updateViews() {
+        
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        
     }
 
 }
