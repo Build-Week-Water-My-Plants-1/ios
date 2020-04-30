32737bb949af0179165c7ef9576c12f2abb10a62
Ezra Black <dev.ezrablack@gmail.com>
2020-04-30 01:29:21 -0500

    Added MockData Loading, Unit Tests, Dependency injection and MockDataLoaders

---
+++ Oxygen/Oxygen/Helpers/TestHelpers.swift
@@ -0,0 +1,15 @@
+//
+//  TestHelpers.swift
+//  Oxygen
+//
+//  Created by Ezra Black on 4/30/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
+
+/// A result type for testing/use case scenarios.
+enum ResultType: String {
+    case success
+    case fail
+}
